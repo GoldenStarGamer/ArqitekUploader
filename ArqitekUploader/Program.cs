2c51114a16473fedaa2285de2b0b3fb46a036f2b
Starman <vicentetavareslima@gmail.com>
2025-03-27 19:34:05 +0000

    added some stuff ig

--- ArqitekUploader/Program.cs
+++ ArqitekUploader/Program.cs
@@ -4,120 +4,129 @@ using System.Text;
 
 internal class Program
 {
-	static void clear()
+	static void Clear()
 	{
 		Console.Clear();
 		Console.WriteLine("Arqitek Upload Tool");
 	}
-	public static void Main(string[] args)
+	public static void Main()
 	{
-		clear();
-		UploadConfig config;
-		if (!File.Exists("config.msgpack"))
+		try
 		{
-			Console.Write("Config file (write $new to make a new config): ");
-
-			string? filepath = null;
-
-			while (filepath is null)
+			Clear();
+			UploadConfig config;
+			if (!File.Exists("config.msgpack"))
 			{
-				filepath = Console.ReadLine();
-			}
+				Console.Write("Config file (write $new to make a new config): ");
 
-			if (filepath is "$new")
-			{
-				config = UploadConfig.CreateConfig();
-				config.Serialize("config.msgpack");
+				string? filepath = null;
+
+				while (filepath is null)
+				{
+					filepath = Console.ReadLine();
+				}
+
+				if (filepath is "$new")
+				{
+					config = UploadConfig.CreateConfig();
+					config.Serialize("config.msgpack");
+				}
+				else
+				{
+					config = UploadConfig.FromFile(filepath);
+				}
 			}
 			else
 			{
-				config = UploadConfig.FromFile(filepath);
+				config = UploadConfig.FromFile("config.msgpack");
 			}
-		}
-		else
-		{
-			config = UploadConfig.FromFile("config.msgpack");
-		}
 
-		string version = "";
+			string version = "";
 
-		if (config.versioning)
-		{
-
-			using (var drive = new Drive())
+			if (config.versioning)
 			{
-				if (drive.FileExists("version.txt"))
+
+				using (var drive = new Drive())
 				{
-					Console.WriteLine($"Previous version: {drive.GetFileContents(drive.FindItem("version.txt")).Result}");
+					if (drive.FileExists("version.txt"))
+					{
+						Console.WriteLine($"Previous version: {drive.GetFileContents(drive.FindItem("version.txt")).Result}");
+					}
+					Console.Write("Version: ");
+					string? _ver = null;
+					while (_ver is null)
+						_ver = Console.ReadLine();
+					version = _ver;
+					File.WriteAllText("version.txt", version);
+					if (config.name is null)
+						drive.UploadFile("version.txt", "text/plain").Wait();
+					else drive.UploadFile("version.txt", "text/plain", drive.FindItem(config.name, true, true)).Wait();
 				}
-				Console.Write("Version: ");
-				string? _ver = null;
-				while (_ver is null)
-					_ver = Console.ReadLine();
-				version = _ver;
-				File.WriteAllText("version.txt", version);
-				if (config.name is null)
-					drive.UploadFile("version.txt", "text/plain").Wait();
-				else drive.UploadFile("version.txt", "text/plain", drive.FindItem(config.name, true, true)).Wait();
+
+				foreach (var item in (from path in config.folds where path is not null select path))
+				{
+					if (config.name is null)
+						File.Copy("version.txt", Path.Combine(item, "version.txt"), true);
+					else File.Copy("version.txt", Path.Combine(item, config.name, "version.txt"), true);
+				}
+
 			}
 
-			foreach (var item in (from path in config.folds where path is not null select path))
-			{
-				if (config.name is null)
-					File.Copy("version.txt", Path.Combine(item, "version.txt"), true);
-				else File.Copy("version.txt", Path.Combine(item, config.name, "version.txt"), true);
-			}
-
-		}
-
-		List<Archive> archives = [];
-		for (int i = 0; i < 3; i++)
-		{
-			if (!string.IsNullOrEmpty(config.folds[i]) && !string.IsNullOrEmpty(config.archives[i]))
-			{
-				archives.Add(new(config.folds[i]!, config.archives[i]!));
-			}
-		}
-
-		clear();
-		List<Task> tasks = [];
-		foreach (var item in archives)
-		{
-			tasks.Add(Task.Run(() => item.Compress(Enprint)));
-		}
-
-		Task.WaitAll(tasks.ToArray());
-
-		clear();
-		using (Drive drive = new())
-		{
-			tasks.Clear();
+			List<Archive> archives = [];
 			for (int i = 0; i < 3; i++)
 			{
-				if (!string.IsNullOrEmpty(config.archives[i]))
+				if (!string.IsNullOrEmpty(config.folds[i]) && !string.IsNullOrEmpty(config.archives[i]))
 				{
-					if (config.name is not null)
-						tasks.Add(drive.UploadFile(config.archives[i]!, "application/x-zip", drive.FindItem(config.name, true, true), Enprint));
-					else tasks.Add(drive.UploadFile(config.archives[i]!, "application/x-zip"));
+					archives.Add(new(config.folds[i]!, config.archives[i]!));
 				}
 			}
-			Task.WaitAll(tasks.ToArray());
+
+			Clear();
+			List<Task> tasks = [];
+			foreach (var item in archives)
+			{
+				tasks.Add(Task.Run(() => item.Compress(Enprint)));
+			}
+
+			Task.WaitAll([.. tasks]);
+
+			Clear();
+			using (Drive drive = new())
+			{
+				tasks.Clear();
+				for (int i = 0; i < 3; i++)
+				{
+					if (!string.IsNullOrEmpty(config.archives[i]))
+					{
+						if (config.name is not null)
+							tasks.Add(drive.UploadFile(config.archives[i]!, "application/x-zip", drive.FindItem(config.name, true, true), Enprint));
+						else tasks.Add(drive.UploadFile(config.archives[i]!, "application/x-zip"));
+					}
+				}
+				Task.WaitAll([.. tasks]);
+			}
+
+			Clear();
+			if (!string.IsNullOrEmpty(config.webhook))
+				SendWebhookMessage(config.webhook, string.IsNullOrEmpty(config.message) ? (string.IsNullOrEmpty(version) ? "A new version has been released" : "Version <version> has been released") : config.message, version ?? string.Empty).Wait();
+
+			Clear();
+			Console.WriteLine("Upload Complete");
+			Console.ReadLine();
+			Console.Clear();
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("ERROR:" + ex.Message);
+			Console.ReadLine();
 		}
 
-		clear();
-		if (!string.IsNullOrEmpty(config.webhook))
-			SendWebhookMessage(config.webhook, string.IsNullOrEmpty(config.message) ? (string.IsNullOrEmpty(version) ? "A new version has been released" : "Version <version> has been released") : config.message, version ?? string.Empty).Wait();
-
-		clear();
-		Console.WriteLine("Upload Complete");
-		Console.ReadLine();
-		Console.Clear();
 	}
 
-	static Dictionary<Archive, int> Arlines = new Dictionary<Archive, int>();
-	static Dictionary<string, int> Stlines = new Dictionary<string, int>();
+	readonly static Dictionary<Archive, int> Arlines = [];
+	readonly static Dictionary<string, int> Stlines = [];
 
-	static object locker = new();
+	static readonly object locker = new();
 
 	// Track the starting cursor position for each line
 	public static void Enprint(float progress, Archive self)
@@ -152,26 +161,24 @@ internal class Program
 		// Create JSON payload
 		string jsonPayload = $"{{\"content\": \"{formattedMessage}\"}}";
 
-		using (HttpClient client = new HttpClient())
+		using HttpClient client = new();
+		try
 		{
-			try
-			{
-				HttpContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-				HttpResponseMessage response = await client.PostAsync(webhookUrl, content);
+			HttpContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+			HttpResponseMessage response = await client.PostAsync(webhookUrl, content);
 
-				if (response.IsSuccessStatusCode)
-				{
-					Console.WriteLine("Webhook message sent successfully.");
-				}
-				else
-				{
-					Console.WriteLine($"Failed to send webhook message. Status code: {response.StatusCode}");
-				}
-			}
-			catch (Exception ex)
+			if (response.IsSuccessStatusCode)
 			{
-				Console.WriteLine($"Error sending webhook message: {ex.Message}");
+				Console.WriteLine("Webhook message sent successfully.");
 			}
+			else
+			{
+				Console.WriteLine($"Failed to send webhook message. Status code: {response.StatusCode}");
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error sending webhook message: {ex.Message}");
 		}
 	}
 
